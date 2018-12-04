980908ca989d839de378e749a5bd1a24f1f5dd7e
Mikey Wan <mikey.wan@outlook.com>
2018-12-05 06:00:14 +0800

    Init
    Demo for dotnetcore

---
+++ Services/InMemoryRestaurantData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OdeToFood.Models;
+
+namespace OdeToFood.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        public InMemoryRestaurantData()
+        {
+            _restaurants = new List<Restaurant>
+            {
+                new Restaurant {Id = 1, Name = "Mikey's Pizza Place"},
+                new Restaurant {Id = 2, Name = "Tersiguels"},
+                new Restaurant {Id = 3, Name = "King's Contrivance"}
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _restaurants.OrderBy(r => r.Name);
+        }
+
+        public Restaurant Get(int id)
+        {
+            return _restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        public Restaurant Add(Restaurant restaurant)
+        {
+            restaurant.Id = _restaurants.Max(r => r.Id) + 1;
+            _restaurants.Add(restaurant);
+            return restaurant;
+        }
+
+        public Restaurant Update(Restaurant restaurant)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<Restaurant> _restaurants;
+    }
+}
